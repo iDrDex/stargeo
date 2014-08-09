4c6ed2b3f1e529c2e2fc6ff37bb37b9ad35a85ef
Dexter Hadley <idrdex@gmail.com>
2014-08-09 13:02:20 -0700

    Fixed to deal with GSE31375 missing data

--- modules/insertStarSamples.py
+++ modules/insertStarSamples.py
@@ -14,7 +14,7 @@ for filename in glob.glob('geo_mirror/DATA/SeriesMatrix/*'):
         if toSkip:
             continue
     gse = Gse(gse_name, doData=False, doSamples=False)
-    if (gse.series.Series_type != "Expression profiling by array").any():
+    if ('Series_type' in gse.series.columns) and (gse.series.Series_type != "Expression profiling by array").any():
         continue
     if (gse.series.Series_sample_taxid != 9606).any():
         continue
