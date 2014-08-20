b4d9a99a571e8b44219330c20e6079d5c16968b6
Dexter Hadley <idrdex@gmail.com>
2014-08-20 14:50:27 -0700

    Speedy queries implemented

--- controllers/default.py
+++ controllers/default.py
@@ -1,13 +1,136 @@
 # -*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
 
-#########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - call exposes all registered services (none by default)
-#########################################################################
+# ########################################################################
+# # This is a sample controller
+# # - index is the default action of any application
+# # - user is required for authentication and authorization
+# # - download is for downloading files uploaded in the db (does streaming)
+# # - call exposes all registered services (none by default)
+# ########################################################################
+
+
+
+#
+# def search_form(self, url):
+# form = FORM('',
+#
+# INPUT(_name='keywords', _value=request.get_vars.keywords,
+#                       _style='width:200px;',
+#                       _id='keywords'),
+#                 INPUT(_type='submit', _value=T('Search')),
+#                 INPUT(_type='submit', _value=T('Clear'),
+#                       _onclick="jQuery('#keywords').val('');"),
+#                 _method="GET", _action=url)
+#
+#     return form
+
+
+import pandas as pd, re
+
+
+def test():
+    fields = [Field('gsm_name'), Field('gse_name')]
+    temp_db = DAL('sqlite:memory').define_table('mytable', *fields)._db
+    temp_db.mytable.truncate()
+    temp_db.mytable.bulk_insert([
+        dict(gsm_name="aaa", gse_name="bbb"),
+        dict(gsm_name="ccc", gse_name="ddd"),
+    ])
+    query = temp_db.mytable.id > 0
+    return dict(grid=SQLFORM.grid(query))
+
+
+def getSearchDf(words):
+    sql = """
+            SELECT gse_name, gpl_name, gsm_name, name, value
+            from sample_attribute_search
+            WHERE
+              doc @@ to_tsquery('simple', '%s');"""
+
+    wordsDf = pd.DataFrame()
+    for word in words:
+        sqlQuery = sql % word
+        rows = db.executesql(sqlQuery, as_dict=True)
+        df = pd.DataFrame.from_records(rows, columns=["gse_name gpl_name gsm_name name value".split()])
+        df['word'] = word
+        df.to_csv("%s.csv" % word)
+        wordsDf = pd.concat([df, wordsDf])
+    return wordsDf
+
+
+def getQuery(keywords):
+    p = re.compile('[\W_]+', re.UNICODE)
+    keywords = set(keywords.split() if keywords else [])
+    print "querying db"
+    df = getSearchDf(keywords)
+    query = Sample_Attribute.id < 0
+    if not df.empty:
+        print "unstacking %s records" % len(df.index)
+        df['name'] = df['name'] \
+            .astype(str) \
+            .apply(lambda x: p.sub("_", x)) \
+            .str.lower()
+        unstacked = df.groupby('gsm_name').filter(lambda row: len(pd.Series.unique(row['word'])) == len(keywords)) \
+            .drop_duplicates(subset=['gsm_name', 'name']) \
+            .set_index(['gsm_name', 'gse_name', 'gpl_name', 'name'])[['value']] \
+            .unstack() \
+            .reset_index()
+        unstacked.columns = list(unstacked.columns.get_level_values(0)[:3]) \
+                            + list(unstacked.columns.get_level_values(1)[3:])
+                            # + ["col%s"%i for i in range(len(unstacked.columns[3:]))]
+        # unstacked = unstacked[list(set(unstacked.columns))]
+        unstacked = unstacked.T.drop_duplicates().T
+        colOrder = sorted(unstacked.columns.tolist(), key = lambda x: len(unstacked[x].dropna()), reverse=True)
+        rowOrder = sorted(unstacked.T.columns.tolist(), key = lambda x: len(unstacked.T[x].dropna()), reverse=True)
+        unstacked = unstacked.T[rowOrder].T[colOrder]
+        unstacked = unstacked.fillna("")
+        unstacked.to_csv('unstacked.csv')
+        recs = unstacked.T.to_dict().values()
+
+        print "inserting %s records" % len(recs)
+        levels = unstacked.columns.get_level_values(1)
+        # Fields to extract from the summary
+        fields = [Field('gsm_name'), Field('gse_name')] \
+                 + [Field('%s' % level) for level in levels]
+
+        temp_db = DAL('sqlite:memory').define_table('mytable', *fields)._db
+        temp_db.mytable.truncate()
+        temp_db.mytable.bulk_insert(recs)
+        query = temp_db.mytable.id > 0
+        print "done"
+    return query
+
+
+QUERY = None
+
+
+def searchable(sfields=None, keywords=""):
+    return QUERY
+
+
+def search():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    # response.flash = T("Welcome to web2py!")
+    # return dict(message=T('Hello World'))
+
+    keywords = request.get_vars.keywords
+    QUERY = getQuery(keywords)
+
+    dashboard = SQLFORM.grid(query=QUERY,
+                             # deletable=False,
+                             # editable=False,
+                             # create=False,
+                             searchable=searchable,
+                             # field_id = 'mytable.id'
+    )
+    return (dict(dashboard=dashboard))
 
 
 def index():
@@ -20,8 +143,10 @@ def index():
     """
     # response.flash = T("Welcome to web2py!")
     # return dict(message=T('Hello World'))
-    grid = SQLFORM.grid(Sample_Attribute)
+
+    grid = SQLFORM.grid(Sample_Attribute.id < 0)
     return dict(grid=grid)
+
 
 @request.restful()
 def api():
@@ -40,6 +165,7 @@ def api():
 
     return locals()
 
+
 def user():
     """
     exposes:
@@ -56,6 +182,7 @@ def user():
     to decorate functions that need access control
     """
     return dict(form=auth())
+
 
 @cache.action()
 def download():
--- models/my_db.py
+++ models/my_db.py
@@ -83,29 +83,3 @@ Search = db.define_table('search',
                          format='%(query)s',
                          migrate='search.table'
 )
-
-
-def create_unique_indices_on_postgres(indices):
-    # Modified from http://osdir.com/ml/web2py/2010-09/msg00952.html
-    '''Creates a set of indices if they do not exist'''
-    # # Edit this list of table columns to index
-    # # The format is [('table', 'column1, column2, ...'),...]
-    print "checking indicies:"
-    for table, columns in indices:
-        relname = "%s_%s_idx"%(table.replace("_", ""),
-                               columns.replace(" ", "") \
-                               .replace("_", "") \
-                               .replace(",", "_"))
-        print "\t%s" % relname,
-        index_exists = \
-            db.executesql("select count(*) from pg_class where relname = '%s';" % relname)[0][0] == 1
-        if not index_exists:
-            print "CREATING"
-            db.executesql('create unique index %s on %s (%s);'
-                          % (relname, table, columns))
-        db.commit()
-        print "OK"
-    print "Done!"
-
-
-create_unique_indices_on_postgres([('sample_attribute', 'sample_id, name')])
