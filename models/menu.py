e5d2bda6051bdd119bf80369b173d4a4729d4dd8
idrdex <idrdex@gmail.com>
2014-09-24 00:58:54 -0700

    Styled the  homepage.  Pulled mobile layout

--- models/menu.py
+++ models/menu.py
@@ -37,10 +37,10 @@ response.google_analytics_id = "UA-42964640-3"
 # ########################################################################
 
 response.menu = [
-    (T('Series'), (request.function == 'index'), URL('series', 'index', f=True, vars=request.vars.keywords and dict(keywords=request.vars.keywords))),
-    (T('Samples'), (request.function == 'index'), URL('sample', 'index', f=True, vars=request.vars.keywords and dict(keywords=request.vars.keywords))),
-    (T('SeriesTags'), (request.function == 'index'), URL('summary', 'index', f=True)),
-    (T('SampleTags'), (request.function == 'index'), URL('annotation', 'index', f=True)),
+    (T('Series'), (request.controller == 'series'), URL('series', 'index', vars=request.vars.keywords and dict(keywords=request.vars.keywords))),
+    (T('Samples'), (request.controller == 'sample'), URL('sample', 'index', vars=request.vars.keywords and dict(keywords=request.vars.keywords))),
+    (T('SeriesTags'), (request.controller == 'summary'), URL('summary', 'index')),
+    (T('SampleTags'), (request.controller == 'annotation'), URL('annotation', 'index')),
 ]
 
 DEVELOPMENT_MENU = False
